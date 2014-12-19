1db8e65ce67106ca4350421b2ea9216917993c9e
Em01 <emma_williams1@hotmail.co.uk>
2014-12-19 16:56:22 +0000

    work on ball class

--- Crazy8Ball.rb
+++ Crazy8Ball.rb
@@ -15,3 +15,32 @@ class Screen
 	end
 
 end
+
+#Define a class representing the 8 ball
+class Ball
+
+	#Define class properties for the 8 ball
+	attr_accessor :randomNo, :greeting, :question, :goodbye
+
+	#Define a method to generate random answers
+	def get_fortune
+		randomNo = 1 + rand(6)
+	end
+
+	#Assign an answer based on the random generated number
+	case randomNo
+
+	when 1
+		$prediction = "yes"
+	when 2
+		$prediction = "no"
+	when 3
+		$prediction = "maybe"
+	when 4
+		$prediction = "hard to tell. Try again"
+	when 5
+		$prediction = "unlikely"
+	when 6
+		$prediction = "unknown"
+	end
+end
