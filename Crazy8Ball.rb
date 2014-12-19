350785c5c2e8d0a1e40cfcbd66fc6119b13c8664
Em01 <emma_williams1@hotmail.co.uk>
2014-12-19 16:49:30 +0000

    work on screen class

--- Crazy8Ball.rb
+++ Crazy8Ball.rb
@@ -1,1 +1,17 @@
 #A game to demonstrate how to work with variables and random numbers to create a fortune telling game that provides randomly selected answers to player questions.
+
+#Define custom classes....................................
+
+#Define a class representing the console window
+class Screen
+
+	def cls #Define a method that clears the area
+		puts("\n" * 25) #Scroll screen 25 times
+		puts "\a" #Beep
+	end
+
+	def pause #Define a method that pauses the display area
+		STDIN.gets #Execute STDIN class's get method to pause script until player presses enter
+	end
+
+end
